b28919b42a44cdfd3f92c3243ab0ee1ceb399308
adrinofrianto <adrinofrianto@gmail.com>
2020-11-06 07:29:55 +0700

    Sound untuk masing-masing huruf pada Screen mendengar sudah berbeda

--- BacaLexia/ViewController/AhurufViewController.swift
+++ BacaLexia/ViewController/AhurufViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class AhurufViewController: UIViewController {
+    
+    var bunyiHuruf : AVAudioPlayer!
 
     @IBOutlet weak var arrowButton: UIImageView!
     var huruf : String = ""
@@ -16,9 +21,17 @@ class AhurufViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         back()
         pilihPic()
+        mendengar()
        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        bunyiHuruf = try! AVAudioPlayer(contentsOf: url!)
+        bunyiHuruf.play()
     }
     
     func back(){
@@ -27,6 +40,14 @@ class AhurufViewController: UIViewController {
         gesture.numberOfTapsRequired = 1
         arrowButton.addGestureRecognizer(gesture)
     }
+    func mendengar(){
+        let gestureDengar =  UITapGestureRecognizer(target: self, action: #selector(hurufImageTapped))
+        hurufImage.isUserInteractionEnabled = true
+        gestureDengar.numberOfTapsRequired = 1
+        hurufImage.addGestureRecognizer(gestureDengar)
+        
+    }
+    
     
     func pilihPic(){
         if huruf == "A"{
@@ -37,6 +58,8 @@ class AhurufViewController: UIViewController {
             hurufImage.image = #imageLiteral(resourceName: "o1")
         }else if huruf == "U"{
             hurufImage.image = #imageLiteral(resourceName: "u")
+        }else if huruf == "E"{
+            hurufImage.image = #imageLiteral(resourceName: "e")
         }
     }
 
@@ -49,6 +72,19 @@ class AhurufViewController: UIViewController {
                 self.present(newViewController, animated: false, completion: nil)
         
     }
-
-
+    
+    @objc func hurufImageTapped(){
+        if huruf == "A"{
+            playSound(soundName: "A")
+        } else if huruf == "O"{
+            playSound(soundName: "O")
+        } else if huruf == "I"{
+            playSound(soundName: "I")
+        } else if huruf == "E"{
+            playSound(soundName: "E")
+        } else if huruf == "U"{
+            playSound(soundName: "U")
+        }
+            
+    }
 }
